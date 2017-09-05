085152e8db153d517ce0b9f9c089fa1c87441293
Kyle Wiering <kawiering@yahoo.com>
2017-09-05 02:01:13 -0500

    Adding Style. Adding unused router.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,14 @@ export class App {
   constructor() {
     this.message = 'Hello World!';
   }
+  configureRouter(config, router){
+    config.title = 'Contacts';
+    config.map([
+      { route: '',              moduleId: 'no-selection',   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name:'contacts' }
+    ]);
+
+    this.router = router;
+  }
+
 }
